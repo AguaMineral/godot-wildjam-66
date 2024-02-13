a18dcea8994eac037058e4f626308483446c0c78
ReLo <m.villa.lore@gmail.com>
2024-02-13 21:47:56 +0100

    interactable object implementation

---
+++ godot/player/player.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+@onready var interact_label = $InteractionComponents/InteractLabel
+
+@onready var all_interactions = []
+
+@export var speed = 400  # speed in pixels/sec
+
+# specify in nodes to load data
+# from save game for this node
+func load_data(data:Dictionary) -> void:
+	print(data.hello) 
+	
+# specify in nodes to save data
+# of this node to the save game
+func save_data() -> Dictionary:
+	return {
+		"hello": "Hello Godot!"
+	}
+
+func _ready():
+	interact_label.text = ""
+
+func _physics_process(delta):
+	handle_movement()
+	handle_input()
+
+func handle_movement():
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
+	
+	move_and_slide()
+
+func handle_input():
+	if Input.is_action_just_pressed("interact"):
+		execute_interaction()
+
+# Interactions
+
+func _on_interaction_area_area_entered(area):
+	all_interactions.insert(0,area)
+	update_interactions()
+
+
+func _on_interaction_area_area_exited(area):
+	all_interactions.erase(area)
+	update_interactions()
+
+func update_interactions():
+	if all_interactions:
+		interact_label.text = all_interactions[0].interact_label
+	else:
+		interact_label.text = ""
+
+func execute_interaction():
+	if all_interactions:
+		var current_interaction = all_interactions[0]
+		match current_interaction.interact_type:
+			"print_text": print(current_interaction.interact_value)
