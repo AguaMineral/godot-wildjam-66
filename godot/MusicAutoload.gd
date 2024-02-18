60ccf5840aaf518fea708f84cd7c053f92b51f9f
ReLo <m.villa.lore@gmail.com>
2024-02-18 07:20:26 +0100

    adding DA music

---
+++ godot/MusicAutoload.gd
@@ -0,0 +1,8 @@
+extends Node2D
+@onready var audio_stream_player = $AudioStreamPlayer
+
+func play():
+	audio_stream_player.play()
+
+func stop():
+	audio_stream_player.stop()
--- godot/scenes/Levels/Puzzle/puzzle_0.gd
+++ godot/scenes/Levels/Puzzle/puzzle_0.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready():
+	MusicAutoload.play()
 
 func _on_first_dialog_timer_timeout():
 	GameManager.play_dialog("dialog_puzzle0_0")
